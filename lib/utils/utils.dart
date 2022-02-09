6a93eac83238bedfa14ae94a1f7a5275e86291cd
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-02-09 13:43:09 +0300

    created snackbar method

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -9,3 +9,11 @@ pickImage(ImageSource source) async {
   }
   print("No image Selected");
 }
+
+showSnackBar(String content, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(content),
+    ),
+  );
+}
