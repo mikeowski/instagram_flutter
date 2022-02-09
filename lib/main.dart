285feb3a4b22657500f72b17b34694a1492ee999
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-02-09 14:24:15 +0300

    change home layout

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       // home: const ResponsiveLayout(
       //     mobileScreenLayout: MobileScreenLayout(),
       //     webScreenLayout: WebScreenLayout())
-      home: SingUpScreen(),
+      home: LoginScreen(),
     );
   }
 }
