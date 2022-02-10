9e4aa231e2a1e80eaabbeb8e37cda760599b382f
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-02-10 23:28:12 +0300

    fix user model

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/screens/login_screen.dart';
-import 'package:instagram_flutter/screens/signup_screen.dart';
+
 import 'package:instagram_flutter/utils/colors.dart';
 
 void main() async {
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,25 +3,27 @@ class User {
   final String email;
   final String bio;
   final String uid;
-  final String fallowers;
+  final List fallowers;
   final List fallowing;
-  final List photoUrl;
+  final String photoUrl;
+
   const User({
-required this.username,
-required this.email,
-required this.bio,
-required this.uid,
-required this.fallowers,
-required this.fallowing,
-required this.photoUrl,
-})
-Map<String, dynamic> toJson() => {
-"username":username,
-"email":email,
-"bio":bio,
-"uid":uid,
-"fallowers":fallowers,
-"fallowing":fallowing,
-"photoUrl":photoUrl
-}
+    required this.username,
+    required this.email,
+    required this.bio,
+    required this.uid,
+    required this.fallowers,
+    required this.fallowing,
+    required this.photoUrl,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "email": email,
+        "bio": bio,
+        "uid": uid,
+        "fallowers": fallowers,
+        "fallowing": fallowing,
+        "photoUrl": photoUrl
+      };
 }
