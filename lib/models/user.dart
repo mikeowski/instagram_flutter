b857bb2b2138e2c4027c03ffaa337956f913ce92
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-02-10 22:06:56 +0300

    create user class

---
+++ lib/models/user.dart
@@ -0,0 +1,27 @@
+class User {
+  final String username;
+  final String email;
+  final String bio;
+  final String uid;
+  final String fallowers;
+  final List fallowing;
+  final List photoUrl;
+  const User({
+required this.username,
+required this.email,
+required this.bio,
+required this.uid,
+required this.fallowers,
+required this.fallowing,
+required this.photoUrl,
+})
+Map<String, dynamic> toJson() => {
+"username":username,
+"email":email,
+"bio":bio,
+"uid":uid,
+"fallowers":fallowers,
+"fallowing":fallowing,
+"photoUrl":photoUrl
+}
+}
