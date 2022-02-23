aa64c78ef17ffc4367135bcd740fa973930a6e2c
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-02-23 22:27:50 +0300

    created feed screen

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post_screen.dart';
+import 'package:instagram_flutter/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Center(child: Text('feed')),
+  FeedScreen(),
   Center(child: Text('search')),
   AddPostScreen(),
   Center(child: Text('favorites')),
