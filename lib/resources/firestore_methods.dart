8ebfb512e43db5a3e05fb7a0370c18ad76632ec6
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-03-06 19:45:32 +0300

    created like post method

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -35,4 +35,21 @@ class FirestoreMethods {
     }
     return res;
   }
+
+  //like post
+  Future<void> likePost(String postId, String uid, List likes) async {
+    try {
+      if (likes.contains(uid)) {
+        await _firestore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayRemove([uid]),
+        });
+      } else {
+        await _firestore.collection('posts').doc(postId).update({
+          'likes': FieldValue.arrayUnion([uid]),
+        });
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
