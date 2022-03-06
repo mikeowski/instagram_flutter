7156c7447a190d4902d902df70df48d72eb2b2d9
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-03-06 16:15:59 +0300

    added Stream builder to watch posts in real time

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_flutter/utils/colors.dart';
@@ -24,7 +25,25 @@ class FeedScreen extends StatelessWidget {
           )
         ],
       ),
-      body: const PostCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) => Container(
+              child: PostCard(
+                snap: snapshot.data!.docs[index].data(),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
