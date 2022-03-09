7740b1f783d88786aa78b7b07832a17f635f3f6f
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2022-03-10 01:13:36 +0300

    change firebase database

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,11 @@ void main() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-          apiKey: "AIzaSyCyB2cUO31Y_A1Vd5cbTmyH4lE1s5BAfws",
-          appId: "1:226145818576:web:0d0baa9365a1ff8ce24bd5",
-          messagingSenderId: "226145818576",
-          projectId: "instagram-clone-b94c0",
-          storageBucket: "instagram-clone-b94c0.appspot.com"),
+          apiKey: "AIzaSyATEADlzMBmiOtUwegNrgdtgqiV4FmWfbM",
+          appId: "1:645903437449:web:ea1c12a9ce93e55f6b0058",
+          messagingSenderId: "645903437449",
+          projectId: "beylerinsta",
+          storageBucket: "beylerinsta.appspot.com"),
     );
   } else {
     await Firebase.initializeApp();
