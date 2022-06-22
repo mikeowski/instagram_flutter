9bc08710371ff9af70e32d39e06edf6229e5157b
Burak Mike <burakmahyk@gmail.com>
2022-06-22 03:00:22 +0300

    added search and profile screens

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post_screen.dart';
 import 'package:instagram_flutter/screens/feed_screen.dart';
 import 'package:instagram_flutter/screens/profile_screen.dart';
+import 'package:instagram_flutter/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
+List<Widget> homeScreenItems = [
   FeedScreen(),
-  Center(child: Text('search')),
+  SearchScreen(),
   AddPostScreen(),
   Center(child: Text('favorites')),
-  ProfileScreen(),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
